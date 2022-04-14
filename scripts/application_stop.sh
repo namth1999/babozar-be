d76ec135a50407e2f617bfb40ea06c14edd387cf
namth1999 <464560@student.saxion.nl>
2022-04-15 00:32:41 +0200

    test cicd2

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install pm2 -g
+pm2 del all
